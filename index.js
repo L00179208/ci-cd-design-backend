c5d9c8e4dbf36999c19010c1f86761adafaadec4
L00179208 <L00179208@atu.ie>
2023-11-23 23:08:38 +0000

    Metrics added for Prom

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/V1", generalRouter);
 app.use("/api/V1/user", userRouter);
 
+// Metrics endpoint
+app.get("/metrics", (req, res) => {
+  res.set("Content-Type", prometheus.register.contentType);
+  res.end(prometheus.register.metrics());
+});
+
+// Define a custom metric
+const customMetric = new prometheus.Gauge({
+  name: "my_custom_metric",
+  help: "A custom metric for my application",
+});
+
+// Increment the custom metric
+customMetric.inc();
+
 const PORT = process.env.PORT || 8111;
 app.listen(PORT, () => {
   console.log("Server has been started at port " + PORT);
