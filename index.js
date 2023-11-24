c3899994661325704586eda9b3aa6f971e6e2986
L00179208 <L00179208@atu.ie>
2023-11-24 00:09:56 +0000

    Removed the Prom

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const userRouter = require("./src/routes/user");
 const generalRouter = require("./src/routes/common");
+const prometheus = require("prom-client");
 
 dotenv.config();
 app.use(cors());
@@ -16,20 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/V1", generalRouter);
 app.use("/api/V1/user", userRouter);
 
-// Metrics endpoint
-app.get("/metrics", (req, res) => {
-  res.set("Content-Type", prometheus.register.contentType);
-  res.end(prometheus.register.metrics());
-});
+// // Metrics endpoint
+// app.get("/metrics", (req, res) => {
+//   res.set("Content-Type", prometheus.register.contentType);
+//   res.end(prometheus.register.metrics());
+// });
 
-// Define a custom metric
-const customMetric = new prometheus.Gauge({
-  name: "my_custom_metric",
-  help: "A custom metric for my application",
-});
+// // Define a custom metric
+// const customMetric = new prometheus.Gauge({
+//   name: "my_custom_metric",
+//   help: "A custom metric for my application",
+// });
 
-// Increment the custom metric
-customMetric.inc();
+// // Increment the custom metric
+// customMetric.inc();
 
 const PORT = process.env.PORT || 8111;
 app.listen(PORT, () => {
